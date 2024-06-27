d1e0f854aedf5a2208ecba8ccc79f4a58a0caa52
Felps-tech <fbocalon74@gmail.com>
2024-06-27 19:21:30 -0400

    Site

--- lojinha/models.py
+++ lojinha/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
@@ -9,7 +8,7 @@ class Product(models.Model):
     price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
-    image = models.ImageField(upload_to='loja/images', default='')
+    image = models.ImageField(upload_to='lojinha/static/lojinha', default='')
     
 def __str__(self):
     return self.product_name
--- lojinha/urls.py
+++ lojinha/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.conf.urls.static import static
 from lojinha import views
+from django.conf import settings
 
-app_name= 'lojinha'
+app_name = 'lojinha'
 urlpatterns = [
-    path('',views.index,name='index.html')
-    
+    path('', views.index, name='index')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
